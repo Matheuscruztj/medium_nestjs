4e19492f1ab2c6dd50842158b11602bbca4f6a3f
matheus.bernardo <matheus.bernardo@compasso.com.br>
2022-09-27 18:13:47 -0300

    feat: class 7 - done

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from './dto/createUser.dto';
@@ -6,6 +6,8 @@ import { UserEntity } from './user.entity';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDTO } from './dto/loginUser.dto';
+import { compare } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -15,11 +17,57 @@ export class UserService {
   ) {}
 
   async createUser(createUserDTO: CreateUserDTO) {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDTO.email,
+    });
+
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDTO.username,
+    });
+
+    if (userByEmail || userByUsername)
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+
     const newUser = new UserEntity();
 
     Object.assign(newUser, createUserDTO);
 
     return await this.userRepository.save(newUser);
+  }
+
+  async login(LoginUserDTO: LoginUserDTO): Promise<UserEntity> {
+    const user = await this.userRepository.findOne(
+      {
+        email: LoginUserDTO.email,
+      },
+      {
+        select: ['id', 'username', 'email', 'bio', 'image', 'password'],
+      },
+    );
+
+    if (!user)
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+
+    const isPasswordCorrect = await compare(
+      LoginUserDTO.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect)
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+
+    delete user.password;
+
+    return user;
   }
 
   generateJwt(user: UserEntity): string {
